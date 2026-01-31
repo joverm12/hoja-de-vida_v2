d82e810f0f16589b477f11d85e0b1e83fbe47cf3
joverm12 <joverjesusmoreiramero@gmail.com>
2026-01-31 15:12:07 -0500

    Creacion automatica de superusuario jover

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# CREACIÓN AUTOMÁTICA DEL SUPERUSUARIO
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='jover').exists() or User.objects.create_superuser('jover', 'joverjesusmoreiramero@gmail.com', '123qwekevin')"
