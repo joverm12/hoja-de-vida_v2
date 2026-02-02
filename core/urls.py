2d9a39013dd02f0e7a07e88a9df965c34180b614
joverm12 <joverjesusmoreiramero@gmail.com>
2026-02-02 10:23:23 -0500

    vamos otra vez 5

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include # Asegúrate de agregar 'include' aquí
+from django.urls import path
 from cv.views import home
 
-# ESTO CUMPLE LO QUE PIDIÓ TU PROFESOR:
-# Solo cambia el Admin, el CV sigue igual.
-admin.site.site_header = "SISTEMA DE GESTIÓN" 
-admin.site.site_title = "Portal Administrativo"
-admin.site.index_title = "Bienvenido"
+# SEGURIDAD: Esto quita la marca "Django" del panel de administración
+admin.site.site_header = "SISTEMA DE GESTIÓN PROFESIONAL" 
+admin.site.site_title = "Panel Administrativo"
+admin.site.index_title = "Bienvenido al Gestor de Contenido"
 
 urlpatterns = [
-    path('admin/', admin.site.urls), # El admin sigue en su ruta
-    # AGREGA ESTA LÍNEA para que el error NoReverseMatch desaparezca:
-    path('accounts/', include('django.contrib.auth.urls')), 
-    path('', home, name='home'),
+    path('admin/', admin.site.urls), # El Admin sigue protegido por su cuenta
+    path('', home, name='home'),     # Tu CV es PÚBLICO (Cualquiera lo ve siempre)
 ]
