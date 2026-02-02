e8b4dd5b72f8e03d12e0244a758d2552005fdd17
joverm12 <joverjesusmoreiramero@gmail.com>
2026-02-02 10:10:38 -0500

    vamos otra vez 3

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from cv.views import home
 
+# PROTECCIÓN DE FOOTPRINTING: Cambia los nombres para que no diga Django
+admin.site.site_header = "SISTEMA DE GESTIÓN PROFESIONAL" 
+admin.site.site_title = "Panel Administrativo" 
+admin.site.index_title = "Bienvenido al Gestor de Contenido"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('admin/', admin.site.urls), # El admin sigue protegido por su propio login
+    path('', home, name='home'),     # Tu CV sigue público para todo el mundo
 ]
